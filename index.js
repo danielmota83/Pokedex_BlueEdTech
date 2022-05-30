5ff5b971fac5cf29b7d311c318ce11c5574bc338
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-05-30 19:51:56 -0300

    Alterações para Deploy

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const port = process.env.PORT || 3000;
+
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(3000, () =>
+app.listen(port, () =>
   console.log("Servidor rodando em http://localhost:3000")
 );
 
